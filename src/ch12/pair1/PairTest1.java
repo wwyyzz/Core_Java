a7924b870a31c2c89ac9233457c6a5308a67b315
wwyyzz <sdn_app@163.com>
2018-11-13 17:52:27 +0800

    ch15

--- src/ch12/pair1/PairTest1.java
+++ src/ch12/pair1/PairTest1.java
@@ -12,14 +12,36 @@ public class PairTest1 {
 
         System.out.println(middle);
 
+        Integer[] nums = {29, 19, 46, 100, 25};
+        Pair<Integer> nn = ArrayAlg.minmax(nums);
+        System.out.println(nn.getFirst());
+        System.out.println(nn.getSecond());
+        System.out.println(ArrayAlg.getMiddle(nums));
+
     }
 }
 
+//class ArrayAlg{
+//    public static Pair<String> minmax(String[] a){
+//        if (a == null || a.length == 0) return null;
+//        String min = a[0];
+//        String max = a[0];
+//        for (int i = 1; i < a.length; i++) {
+//            if (min.compareTo(a[i]) > 0) min = a[i];
+//            if (max.compareTo(a[i]) < 0) max = a[i];
+//        }
+//        return new Pair<>(min, max);
+//    }
+//
+//    public static <T extends Comparable> T getMiddle(T... a){
+//        return a[a.length / 2];
+//    }
+//}
 class ArrayAlg{
-    public static Pair<String> minmax(String[] a){
+    public static <T extends Comparable> Pair<T> minmax(T[] a){
         if (a == null || a.length == 0) return null;
-        String min = a[0];
-        String max = a[0];
+        T min = a[0];
+        T max = a[0];
         for (int i = 1; i < a.length; i++) {
             if (min.compareTo(a[i]) > 0) min = a[i];
             if (max.compareTo(a[i]) < 0) max = a[i];
@@ -27,7 +49,7 @@ class ArrayAlg{
         return new Pair<>(min, max);
     }
 
-    public static <T> T getMiddle(T... a){
+    public static <T extends Comparable> T getMiddle(T... a){
         return a[a.length / 2];
     }
 }
---
+++ src/ch12/pair1/pair2.java
@@ -0,0 +1,4 @@
+package ch12.pair1;
+
+public class pair2 {
+}
