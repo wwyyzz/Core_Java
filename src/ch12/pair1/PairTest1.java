525fb026fb288505b5d334da3382ac671e97379c
wwyyzz <sdn_app@163.com>
2018-11-13 17:27:55 +0800

    ch15

---
+++ src/ch12/pair1/PairTest1.java
@@ -0,0 +1,33 @@
+package ch12.pair1;
+
+
+public class PairTest1 {
+    public static void main(String[] args) {
+        String[] words = {"mary", "had", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minmax(words);
+        String middle = ArrayAlg.getMiddle(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+
+
+        System.out.println(middle);
+
+    }
+}
+
+class ArrayAlg{
+    public static Pair<String> minmax(String[] a){
+        if (a == null || a.length == 0) return null;
+        String min = a[0];
+        String max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) min = a[i];
+            if (max.compareTo(a[i]) < 0) max = a[i];
+        }
+        return new Pair<>(min, max);
+    }
+
+    public static <T> T getMiddle(T... a){
+        return a[a.length / 2];
+    }
+}
