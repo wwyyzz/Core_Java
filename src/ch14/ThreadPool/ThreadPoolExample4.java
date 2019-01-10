c8264743e894982d785cef5f73f64cb09e42c1b6
wangjun <wwyyzz08@sina.com>
2019-01-10 23:30:26 +0800

    0106

---
+++ src/ch14/ThreadPool/ThreadPoolExample4.java
@@ -0,0 +1,22 @@
+package ch14.ThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExample4 {
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+        executorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hi" + System.currentTimeMillis());
+            }
+        },3,TimeUnit.SECONDS);
+
+
+        executorService.shutdown();
+    }
+}
